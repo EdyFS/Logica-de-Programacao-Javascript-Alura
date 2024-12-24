d7d2a37645442d0c54bd0a996514e2a47cc526cb
EdyFS <edy.ferreira.santana@gmail.com>
2024-12-24 15:09:48 -0300

    Adicionando funções do sorteador de números

--- Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,8 +1,26 @@
-function sortear(){
+let quantidade = document.getElementById('quantidade');
+let de = document.getElementById('de');
+let ate = document.getElementById('ate');
+let resposta = [];
 
+function sortear(de, ate){
+    for(let i = 0; i < quantidade; i++){
+        let numero = parseInt(Math.random() * (ate - de)+ de);
+        console.log(numero);
+        resposta.push(numero);
+    }
+    //let texto = `Números sorteados: ${resposta = [i]}`;
+    document.getElementById('resultado').innerHTML = `Números sorteados: ${resposta}`;
+    console.log(resposta);
+    //console.log(texto);
 }
 
-
 function reiniciar(){
-    
+    quantidade = document.getElementById('quantidade');
+    quantidade.value = '';
+    de = document.getElementById('de');
+    de.value = '';
+    ate = document.getElementById('ate');
+    ate.value = '';
+    document.getElementById('resultado').innerHTML = 'Números sorteados:  nenhum até agora';
 }
