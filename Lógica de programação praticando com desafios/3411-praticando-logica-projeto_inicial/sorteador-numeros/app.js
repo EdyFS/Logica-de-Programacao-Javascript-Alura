1bf9ef4ec3784fb044b117a64c3407e3bfbc8417
EdyFS <edy.ferreira.santana@gmail.com>
2024-12-24 17:45:23 -0300

    Adicionando funções dos instrutores alura

--- Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,15 +1,10 @@
-/*let quantidade = document.getElementById('quantidade').value;
-let de = document.getElementById('de').value;
-let ate = document.getElementById('ate').value;
-let resposta = [];*/
-
 function sortear(){
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
     let resposta = [];
     for(let i = 0; i < quantidade; i++){
-        let numero = parseInt(Math.random() * (ate - de)+ de);
+        let numero = parseInt(Math.random() * (ate - de + 1)+ de);
         console.log(numero);
         resposta.push(numero);
     }
@@ -17,6 +12,7 @@ function sortear(){
     document.getElementById('resultado').innerHTML = `Números sorteados: ${resposta}`;
     console.log(resposta);
     //console.log(texto);
+    alterarStatusBotao();
 }
 
 function reiniciar(){
@@ -27,4 +23,59 @@ function reiniciar(){
     ate = document.getElementById('ate');
     ate.value = '';
     document.getElementById('resultado').innerHTML = 'Números sorteados:  nenhum até agora';
+    alterarStatusBotao();
 }
+
+/*Resposta da ALURA
+
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    //alert(`Quantidade: ${quantidade}`);
+    //alert(`Do número: ${de}`);
+    //alert(`Até o número: ${ate}`);
+
+    let sorteados = [];
+    
+    let numero;
+    //alert(numero);
+    
+    for(let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(de, ate);
+        while(sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
+    }
+        alert(sorteados)
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class = "texto_paragrafo">Números sorteados: ${sorteados}</label>`
+    alterarStatusBotao();
+}
+
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }
+    else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    quantidade = document.getElementById('quantidade').value = '';
+    de = document.getElementById('de').value = '';
+    ate = document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = <label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>;
+    alterarStatusBotao();
+}
+    */
