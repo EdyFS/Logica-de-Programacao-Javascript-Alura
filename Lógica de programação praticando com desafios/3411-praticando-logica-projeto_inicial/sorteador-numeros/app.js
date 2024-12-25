85f0805993d26ec551acb7d0cef2648768294f74
EdyFS <edy.ferreira.santana@gmail.com>
2024-12-25 15:06:22 -0300

    Incorporando código alura ao meu código

--- Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -5,11 +5,15 @@ function sortear(){
     let resposta = [];
     for(let i = 0; i < quantidade; i++){
         let numero = parseInt(Math.random() * (ate - de + 1)+ de);
+        while(resposta.includes(numero)){
+            numero = parseInt(Math.random() * (ate - de + 1)+ de);
+        }
         console.log(numero);
         resposta.push(numero);
     }
     //let texto = `Números sorteados: ${resposta = [i]}`;
-    document.getElementById('resultado').innerHTML = `Números sorteados: ${resposta}`;
+    let texto = document.getElementById('resultado');
+    texto.innerHTML = `<label class = "texto__paragrafo">Números sorteados: ${resposta}</label>`;
     console.log(resposta);
     //console.log(texto);
     alterarStatusBotao();
@@ -22,8 +26,21 @@ function reiniciar(){
     de.value = '';
     ate = document.getElementById('ate');
     ate.value = '';
-    document.getElementById('resultado').innerHTML = 'Números sorteados:  nenhum até agora';
+    texto = document.getElementById('resultado');
+    texto.innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
     alterarStatusBotao();
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }
+    else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
 }
 
 /*Resposta da ALURA
