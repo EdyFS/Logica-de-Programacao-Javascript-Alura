6c863b18578e9a00137ad96659e68a924d697cc1
EdyFS <edy.ferreira.santana@gmail.com>
2024-12-27 18:54:02 -0300

    Desafios

--- Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/alugames/js/app.js
+++ Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/alugames/js/app.js
@@ -44,22 +44,47 @@ function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    //let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
     //alert(nomeJogo.textContent);
-    if (botao.classList.contains('dashboard__item__button--return' )){
-        //botao.classList.remove('dashboard__item__button');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.innerHTML = 'Alugar';
+    if (imagem.classList.contains('dashboard__item__img--rented')){
+        let confirmacao = prompt('Para devolver digite "DEVOLVER": ').toUpperCase();
+        console.log(confirmacao);
+        //if (confirm(`Você tem certeza que deseja devolver o jofo ${nomeJogo.textContent}?`))
+        if (confirmacao == "DEVOLVER"){
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            imagem.classList.remove('dashboard__item__img--rented');
+            //contador--;
+        }
     }
     else{
         botao.classList.add('dashboard__item__button--return');
-        //botao.classList.add('dashboard__item__button');
-        botao.innerHTML = 'Devolver';
+        botao.textContent = 'Devolver';
+        imagem.classList.add('dashboard__item__img--rented');
+        //contador++;
     }
-    if (imagem.classList.contains('dashboard__item__img--rented')){
+    /*if (imagem.classList.contains('dashboard__item__img--rented')){
         imagem.classList.remove('dashboard__item__img--rented');
     }
     else{
         imagem.classList.add('dashboard__item__img--rented');
-    }
+    }*/
+   contarAlugados()
 }
+
+function contarAlugados(){
+    let contador = 0;
+    for (id = 1; id <= 3; id++){
+        gameClicado = document.getElementById(`game-${id}`);
+        imagem = gameClicado.querySelector('.dashboard__item__img');
+        if (imagem.classList.contains('dashboard__item__img--rented')){
+            contador = contador + 1;
+        }
+    }
+    console.log(`${contador} jogos estão alugados`);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    contador = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarAlugados();
+});
