0711a7e2b1ab9c808dd14af2b60583295f478458
EdyFS <edy.ferreira.santana@gmail.com>
2025-01-03 21:48:52 -0300

    Solução Alura

--- Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/ingresso/js/app.js
+++ Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/ingresso/js/app.js
@@ -35,7 +35,59 @@ function comprar(){
     inferior.innerHTML = `<span id="qtd-inferior">${qtdInferior}</span>`;
 }
 
+/*
 //Solução Alura
 function comprar(){
-    
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if(tipo.value == 'pista'){
+        comprarPista(qtd);
+    }
+    else if(tipo.value == 'superior'){
+        comprarSuperior(qtd);
+    }
+    else if(tipo.value == 'inferior'){
+        comprarInferior(qtd);
+    }
 }
+
+function comprarPista(){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista){
+        alert('Quantidade indisponível para tipo pista');
+    }
+    else{
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizado com sucesso');
+
+    }
+}
+
+function comprarSuperior(){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior){
+        alert('Quantidade indisponível para tipo superior');
+    }
+    else{
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizado com sucesso');
+
+    }
+}
+
+function comprarInferior(){
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior){
+        alert('Quantidade indisponível para tipo inferior');
+    }
+    else{
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizado com sucesso');
+
+    }
+}
+    */
