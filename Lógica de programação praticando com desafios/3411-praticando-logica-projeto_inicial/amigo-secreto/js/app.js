c7be89304250ed5c11836010df91bec5da7bbd63
EdyFS <edy.ferreira.santana@gmail.com>
2025-01-04 22:26:10 -0300

    Solução Alura

--- Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ Lógica de programação praticando com desafios/3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -3,14 +3,13 @@ let nome;
 let lista;
 
 function adicionar(){
-    nome = document.getElementById('nome-amigo').value;
-    console.log(nome);
-    listaNomes.push(nome);
+    nome = document.getElementById('nome-amigo');
+    console.log(nome.value);
+    listaNomes.push(nome.value);
     console.log(listaNomes);
     lista = document.getElementById('lista-amigos');
     lista.textContent = listaNomes;
-    nome.value = 'Nome do amigo';
-    nome.innerHTML = 'Nome do amigo';
+    nome.value = '';
 }
 
 function sortear(){
@@ -44,9 +43,65 @@ function sortear(){
 function reiniciar(){
     nome.value = '';
     console.log(nome);
-    for(let i = 0; i <= listaNomes.length; i++){
+    /*for(let i = 0; i <= listaNomes.length; i++){
         listaNomes.pop(i);
-    }
+    }*/
+    listaNomes = [];
     lista.textContent = '';
+    document.getElementById('nome-amigo').textContent = '';
     console.log(listaNomes);
 }
+
+function remover(){
+    listaNomes.pop();
+}
+/*
+//Solução Alura
+let amigos = [];
+
+function adicionar(){
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+    amigos.push(amigo.value);
+    if (lista.textContent == ''){
+        lista.textContent = amigo.value;
+    }
+    else{
+        lista.textContent = lista.textContent + ',' + amigo.value;
+    }
+    amigo.value = '';
+
+}
+
+function sortear(){
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++){
+        if (i == amigos.length - 1){
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[0] + '<br>';
+        }
+        else{
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[i+1] + '<br>';
+        }
+        
+    }
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar(){
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
+*/
